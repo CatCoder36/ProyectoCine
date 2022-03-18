716037d1eb6fdb3c8e65405595863e458711bb7c
Eduard herrera <eduardjhonnherrera@gmail.com>
2022-03-18 05:59:21 -0400

    Añadiendo la clase Banco y Banco Test

--- src/Banco.java
+++ src/Banco.java
@@ -1,2 +1,155 @@
+import java.util.ArrayList;
+
 public class Banco {
+
+    public static String username;
+    public int claveSecreta;
+    public String fechaAfiliacion;
+    public int montoInicial;
+    private boolean bancoWeb;
+    public String nombreBancoAfiiado;
+//    ArrayList<Integer> fechaAfiliacion;
+
+    ArrayList<String> listaBancosDisponibles; // nombre de tarjetas de crédito
+    int operacionesRealizadas; // contador
+    int gananciaBruta;
+    int gananciaTotal;
+    int saldoTotal;
+    double impuestos = 0.13;
+
+
+    // Constructor (Creando nuestro banco afiliado)
+    public Banco(String username,int claveSecreta, String fechaAfiliacion, int montoInicial, boolean bancoWeb,
+                 String nombreBancoAfiiado)
+    {
+        this.username = username;
+        this.claveSecreta = claveSecreta;
+        this.fechaAfiliacion = fechaAfiliacion;
+        this.montoInicial = montoInicial;
+        this.bancoWeb = bancoWeb;
+        this.nombreBancoAfiiado = nombreBancoAfiiado;
+    }
+
+    // Métodos
+
+    //Caja
+    public void addListaBancosDisponibles()
+    {
+        listaBancosDisponibles.add("Elefante");
+        listaBancosDisponibles.add("BCP");
+        listaBancosDisponibles.add("Banco Sol");
+        listaBancosDisponibles.add("Banco Fácil");
+        listaBancosDisponibles.add("Banco Visa");
+    }
+
+    public boolean verificarListaBancosDisponibles(String nombreBanco) // ***
+    {
+        boolean verificador = true;
+        for(int i = 0; i <= listaBancosDisponibles.size(); i++)
+        {
+            if (nombreBanco == listaBancosDisponibles.get(i))
+            {
+                verificador = true;
+            }
+            else
+            {
+                verificador = false;
+            }
+        }
+        return verificador;
+    }
+
+    public int obtenerGananciaBruta(int precioPelicula) // ***
+    {
+        gananciaBruta += precioPelicula;
+        return gananciaBruta;
+    }
+
+    public int obtenerGananciaTotal(int precioPelicula) // ***
+    {
+        gananciaTotal += precioPelicula;
+        double aplicacionImpuestos = gananciaTotal * impuestos;
+        gananciaTotal -= aplicacionImpuestos;
+        return gananciaTotal;
+    }
+
+    public boolean activarCuenta(String username, int claveSecreta) // ***
+    {
+        if(username == this.username && claveSecreta == this.claveSecreta)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public String depositar(int deposito) // ***
+    {
+        String ingresoIncorrecto = "Ingreso Incorrecto";
+        String ingresoCorrecto = "Ingreso Correcto";
+        if(deposito < 0)
+        {
+            return ingresoIncorrecto;
+        }
+        else
+        {
+            saldoTotal += deposito;
+            this.operacionesRealizadas += 1;
+            return ingresoCorrecto;
+        }
+    }
+
+    public void retirar(int retiro) // *** // Devolver cambio
+    {
+        if(retiro > saldoTotal)
+        {
+            String respuestaSaldoDisponible = "No tiene ese saldo disponible";
+            System.out.println(respuestaSaldoDisponible);
+        }
+        else
+        {
+            saldoTotal -= retiro;
+            this.operacionesRealizadas += 1;
+        }
+    }
+
+    @Override // Factura o Estado de cuenta // ***
+    public String toString() {
+        return "Banco{" +
+                "fechaAfiliacion='" + fechaAfiliacion + '\'' +
+                ", bancoWeb=" + bancoWeb +
+                ", nombreBancoAfiiado='" + nombreBancoAfiiado + '\'' +
+                ", operacionesRealizadas=" + operacionesRealizadas +
+                ", gananciaBruta=" + gananciaBruta +
+                ", gananciaTotal=" + gananciaTotal +
+                ", saldoTotal=" + saldoTotal +
+                '}';
+    }
+
+    public boolean getBancoWeb()
+    {
+        return bancoWeb;
+    }
+
+    public int getPassword()
+    {
+        return claveSecreta;
+    }
+
+    public String getFechaAfiliacion()
+    {
+        return fechaAfiliacion;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getNombreBancoAfiliado() {
+        return nombreBancoAfiiado;
+    }
+
 }
+
---
+++ test/TestBanco.java
@@ -0,0 +1,89 @@
+import junit.framework.TestCase;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.assertEquals;
+import java.util.ArrayList;
+
+
+public class TestBanco extends TestCase {
+
+
+    // Instancias de Banco
+    Banco banco1 = new Banco("Cine OOP", 159, "01/01/2020",1500,true,
+            "Banco Elefante");
+    Banco banco2 = new Banco("Cine OOP", 259, "01/01/2020",1500,false,
+            "Banco Visa");
+    Banco banco3 = new Banco("Cine OOP", 359, "01/01/2020",1500,true,
+            "Banco Union");
+
+    ArrayList<Banco> bancosAfiliados = new ArrayList<>();
+
+    @Test
+    public void verificarCantidadBancosAfiliados()
+    {
+        bancosAfiliados.add(banco1);
+        bancosAfiliados.add(banco2);
+        bancosAfiliados.add(banco3);
+        assertEquals(3, bancosAfiliados.size());
+    }
+
+    @Test
+    public void verificarUsernameBancoAfiliado()
+    {
+        String usernameBanco1 = banco1.getUsername();
+        String usernameBanco2 = banco2.getUsername();
+        String usernameBanco3 = banco3.getUsername();
+
+        assertEquals("Cine OOP", usernameBanco1);
+        assertEquals("Cine OOP", usernameBanco2);
+        assertEquals("Cine OOP", usernameBanco3);
+    }
+
+    @Test
+    public void verificarClaveSecretaBancoAfiliado()
+    {
+        int passwordBanco1 = banco1.getPassword();
+        int passwordBanco2 = banco2.getPassword();
+        int passwordBanco3 = banco3.getPassword();
+
+        assertEquals(159, passwordBanco1);
+        assertEquals(259, passwordBanco2);
+        assertEquals(359, passwordBanco3);
+    }
+
+    @Test
+    public void verificarFechaAfiliacion()
+    {
+        String fechaAfiliacionBanco1 = banco1.getFechaAfiliacion();
+        String fechaAfiliacionBanco2 = banco2.getFechaAfiliacion();
+        String fechaAfiliacionBanco3 = banco3.getFechaAfiliacion();
+
+        assertEquals("01/01/2020", fechaAfiliacionBanco1);
+        assertEquals("01/01/2020", fechaAfiliacionBanco2);
+        assertEquals("01/01/2020", fechaAfiliacionBanco3);
+    }
+
+    @Test
+    public void verificarBancaWeb() // Si es permitido las transacciones internacionales
+    {
+        boolean bancaWebBanco1 = banco1.getBancoWeb();
+        boolean bancaWebBanco2 = banco2.getBancoWeb();
+        boolean bancaWebBanco3 = banco3.getBancoWeb();
+
+        assertEquals(true, bancaWebBanco1);
+        assertEquals(false, bancaWebBanco2);
+        assertEquals(true, bancaWebBanco3);
+    }
+
+    @Test
+    public void verificarNombreBancoAfiliado()
+    {
+        String nombreAfiliadoBanco1 = banco1.getNombreBancoAfiliado();
+        String nombreAfiliadoBanco2 = banco2.getNombreBancoAfiliado();
+        String nombreAfiliadoBanco3 = banco3.getNombreBancoAfiliado();
+
+        assertEquals("Banco Elefante", nombreAfiliadoBanco1);
+        assertEquals("Banco Visa", nombreAfiliadoBanco2);
+        assertEquals("Banco Union", nombreAfiliadoBanco3);
+    }
+
+}
